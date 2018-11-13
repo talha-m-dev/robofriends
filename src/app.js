21fe7cae3ced4de8507a60e1b9eabb0d07dd6bc5
hth_developers <talhamanzoor006@gmail.com>
2018-11-13 22:55:23 +0500

    update

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ class App extends Component{
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=> response.json())
-        .then(users=>this.setState( {robots: users }));
+        .then(users=>this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
