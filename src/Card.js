1faff921c56a7d7b25816980681602674eb2b8f0
hth_developers <talhamanzoor006@gmail.com>
2018-11-13 22:46:12 +0500

    modified

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email, id} ) => {
 	return (
 		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'> 
-			<img alt='robots' src={`https://robohash.org/${id}?100x100`}/>
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ class App extends Component{
     }
 
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users').then(response=> response.json()).then(users=>{
-            this.setState( {robots: users })
-        })
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=>this.setState( {robots: users }));
     }
 
     onSearchChange = (event) => {
